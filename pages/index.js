0f5bac96738ee2a41d54240f0a13c2e0b498217e
richardsinaga261@gmail.com <richardsinaga261@gmail.com>
2022-08-18 14:17:45 +0700

    a

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,24 @@ import styles from "../styles/Home.module.css";
 import Banner from "../components/banner/banner";
 import NavBar from "../components/nav/navbar";
 import Card from "../components/card/card";
+import SectionCards from "../components/card/section-cards";
 
-export default function Home() {
+import { getVideos } from "../lib/videos";
+
+export async function getServerSideProps() {
+  const disneyVideos = await getVideos("disney trailer");
+  const productivityVideos = await getVideos("productivity");
+  const travelVideos = await getVideos("travel");
+  // const popularVideos = await getVideos("disney trailer");
+  return { props: { disneyVideos, travelVideos, productivityVideos } };
+}
+
+export default function Home({
+  disneyVideos,
+  travelVideos,
+  productivityVideos,
+}) {
+  console.log({ disneyVideos });
   return (
     <div className={styles.container}>
       <Head>
@@ -20,9 +36,17 @@ export default function Home() {
         subTitle="a very cute dog"
         imgUrl="/static/clifford.webp"
       />
-      <Card imgUrl="/static/clifford.webp" size="large" />
-      <Card size="medium" />
-      <Card imgUrl="/static/clifford.webp" size="small" />
+
+      <div className={styles.sectionWrapper}>
+        <SectionCards title="Disney" videos={disneyVideos} size="large" />
+        <SectionCards title="Travel" videos={travelVideos} size="small" />
+        <SectionCards
+          title="Productivity"
+          videos={productivityVideos}
+          size="medium"
+        />
+        <SectionCards title="Popular" videos={disneyVideos} size="small" />
+      </div>
     </div>
   );
 }
