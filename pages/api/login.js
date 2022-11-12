d719fbd0c15d4243083a86c7620e7aed91ee6892
richardsinaga261@gmail.com <richardsinaga261@gmail.com>
2022-11-12 21:13:05 +0700

    check new user

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,17 +1,35 @@
 import { magicAdmin } from "../../lib/magic";
+import jwt from "jsonwebtoken";
+import { isNewUser } from "../../lib/db/hasura";
 
 export default async function login(req, res) {
   if (req.method === "POST") {
     try {
       const auth = req.headers.authorization;
       const didToken = auth ? auth.substr(7) : "";
-      console.log({ didToken });
-
+      // invoke magic
       const metadata = await magicAdmin.users.getMetadataByToken(didToken);
       console.log({ metadata });
+      // create jwt
+      const token = jwt.sign(
+        {
+          ...metadata,
+          iat: Math.floor(Date.now() / 1000),
+          exp: Math.floor(Date.now() / 1000 + 7 * 24 * 60 * 60),
+          "https://hasura.io/jwt/claims": {
+            "x-hasura-allowed-roles": ["user", "admin"],
+            "x-hasura-default-role": "user",
+            "x-hasura-user-id": `${metadata.issuer}`,
+          },
+        },
+        "thisisasecrettthisisasecrett1234"
+      );
+      console.log({ token });
 
-      //invoke magic
-      res.send({ done: true });
+      // CHECK IF USER EXISTS
+
+      const isNewUserQuery = await isNewUser(token);
+      res.send({ done: true, isNewUserQuery });
     } catch (error) {
       console.error("Something went wrong logging in", error);
       res.status(500).send({ done: false });
