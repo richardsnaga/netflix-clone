f88417337060cc7e429483c9ece3109f86522711
richardsinaga261@gmail.com <richardsinaga261@gmail.com>
2022-11-13 20:22:27 +0700

    Use Query String For Video ID

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -1,17 +1,20 @@
 import jwt from "jsonwebtoken";
-
+import { findVideoIdByUser } from "../../lib/db/hasura";
 export default async function stats(req, resp) {
   if (req.method === "POST") {
     console.log({ cookies: req.cookies });
-
     try {
       const token = req.cookies.token;
       if (!token) {
         resp.status(403).send({});
       } else {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        console.log({ decoded });
-        resp.send({ msg: "it works", decoded });
+        const videoId = req.query.videoId;
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+        console.log({ decodedToken });
+
+        const userId = decodedToken.issuer;
+        const findVideoId = await findVideoIdByUser(token, userId, videoId);
+        resp.send({ msg: "it works", decodedToken, findVideoId });
       }
     } catch (error) {
       console.error("Error occurred /stats", error);
