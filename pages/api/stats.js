14902c32b102cf1b902242b7e29485a0e3938c5c
richardsinaga261@gmail.com <richardsinaga261@gmail.com>
2022-11-14 21:36:43 +0700

    stats get

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -1,9 +1,10 @@
 import jwt from "jsonwebtoken";
 import {
   findVideoIdByUser,
-  insertStats,
   updateStats,
+  insertStats,
 } from "../../lib/db/hasura";
+
 export default async function stats(req, resp) {
   if (req.method === "POST") {
     try {
@@ -11,17 +12,15 @@ export default async function stats(req, resp) {
       if (!token) {
         resp.status(403).send({});
       } else {
-        const { videoId, favourited, watched = true } = req.body;
-
+        const { videoId } = req.body;
         if (videoId) {
           const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
 
           const userId = decodedToken.issuer;
-          const doesStatsExist = await findVideoIdByUser(
-            token,
-            userId,
-            videoId
-          );
+          const findVideo = await findVideoIdByUser(token, userId, videoId);
+          const doesStatsExist = findVideo?.length > 0;
+
+          const { favourited, watched = true } = req.body;
           if (doesStatsExist) {
             // update it
             const response = await updateStats(token, {
@@ -30,7 +29,7 @@ export default async function stats(req, resp) {
               videoId,
               favourited,
             });
-            resp.send({ msg: "it works", response });
+            resp.send({ data: response });
           } else {
             // add it
             const response = await insertStats(token, {
@@ -39,13 +38,36 @@ export default async function stats(req, resp) {
               videoId,
               favourited,
             });
-            resp.send({ msg: "it works", response });
+            resp.send({ data: response });
           }
+        } else {
+          resp.status(500).send({ msg: "videoId is required" });
         }
       }
     } catch (error) {
       console.error("Error occurred /stats", error);
       resp.status(500).send({ done: false, error: error?.message });
+    }
+  } else {
+    const token = req.cookies.token;
+    if (!token) {
+      resp.status(403).send({});
+    } else {
+      const { videoId } = req.body;
+      if (videoId) {
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+        const userId = decodedToken.issuer;
+        const findVideo = await findVideoIdByUser(token, userId, videoId);
+        const doesStatsExist = findVideo?.length > 0;
+        if (doesStatsExist) {
+          resp.send(findVideo);
+        } else {
+          // add it
+          resp.status(404);
+          resp.send({ user: null, msg: "Video not found" });
+        }
+      }
     }
   }
 }
