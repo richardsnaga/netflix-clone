b0b76a26faa411607d03a06a98eca3cd754af9a7
richardsnaga <55023608+richardsnaga@users.noreply.github.com>
2025-09-02 23:04:59 +0700

    Comment for bypass login

--- utils/redirectUser.js
+++ utils/redirectUser.js
@@ -2,7 +2,8 @@ import { verifyToken } from "../lib/utils";
 
 export const redirectUser = async (context) => {
   const token = context.req ? context.req.cookies?.token : null;
-  const userId = await verifyToken(token);
+  // const userId = await verifyToken(token);
+  const userId = "1";
 
   return {
     userId,
