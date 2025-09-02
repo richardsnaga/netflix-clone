cb2acca51e8bcd3a852fbc96e7d1c32bf5f84e01
richardsnaga <55023608+richardsnaga@users.noreply.github.com>
2025-09-02 22:57:21 +0700

    Comment for bypass login

--- pages/browse/my-list.js
+++ pages/browse/my-list.js
@@ -8,15 +8,15 @@ import styles from "../../styles/MyList.module.css";
 export async function getServerSideProps(context) {
   const { userId, token } = await redirectUser(context);
 
-  if (!userId) {
-    return {
-      props: {},
-      redirect: {
-        destination: "/login",
-        permanent: false,
-      },
-    };
-  }
+  // if (!userId) {
+  //   return {
+  //     props: {},
+  //     redirect: {
+  //       destination: "/login",
+  //       permanent: false,
+  //     },
+  //   };
+  // }
   const videos = await getMyList(userId, token);
 
   return {
